b39730b1119a98e82b0ee7e053c35581752e6a9a
unknown <matthew.teabear.basford@gmail.com>
2024-05-06 03:53:05 +0100

    added README and removed currently unused env variables

---
+++ README.md
@@ -0,0 +1,16 @@
+## Setup
+- Create your own .env file with your dev discord bot's client and token
+- Install pnpm via npm
+```npm install -g pnpm```
+
+- Install tsx
+```pnpm install -D tsx```
+
+- Install other packages
+```pnpm install```
+
+
+## Running the program
+```tsx src/bot.ts```\
+**or**\
+```tsx watch src/bot.ts``` (restarts upon file changes)
--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID, SUPABASE_PUBLIC_KEY, SUPABASE_URL } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !SUPABASE_PUBLIC_KEY || !SUPABASE_URL) {
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
 	throw new Error("Missing environment variables");
 }
 
 export const config = {
 	DISCORD_TOKEN,
-	DISCORD_CLIENT_ID,
-	SUPABASE_PUBLIC_KEY,
-	SUPABASE_URL
+	DISCORD_CLIENT_ID
 };
