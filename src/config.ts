1d3df1147c9887959a282420ec7b5962e248c941
unknown <matthew.teabear.basford@gmail.com>
2024-05-06 19:36:06 +0100

    added supabase support

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, SUPABASE_URL, SUPABASE_PUBLIC_KEY } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !SUPABASE_URL || !SUPABASE_PUBLIC_KEY) {
 	throw new Error("Missing environment variables");
 }
 
 export const config = {
 	DISCORD_TOKEN,
-	DISCORD_CLIENT_ID
+	DISCORD_CLIENT_ID,
+	SUPABASE_URL,
+	SUPABASE_PUBLIC_KEY
 };
